124c9c6d8482b970582d01fcc81fff31f36da97c
Gin <lingin1230@gmail.com>
2021-01-04 21:11:55 +0800

    v1

---
+++ MakeSound.js
@@ -0,0 +1,21 @@
+
+var a = function (playA) {
+
+  document.querySelector('.sound1').audio.play;
+
+}
+
+document.querySelector ('.key1').addEventListener ( 'keypress' ,  );
+
+
+document.addEventListener ( 'keypress' ,function() {
+  if (event.keycode === 65 || event.which === 65) {
+    addItem();
+  }
+});
+
+
+
+
+
+// document.querySelector (DOMstrings.btn).addEventListener ( 'click' , addItem );
